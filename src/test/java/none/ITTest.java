47b53ed2cc03ba6cca0083c95ed352b54a80ff9a
peng-yongsheng <pengys@apache.org>
2018-12-15 22:19:07 +0800

    Provide three maven profiles, e.g., 2.4.6, 5.6.14, 6.5.3 for integration test.

---
+++ src/test/java/none/ITTest.java
@@ -0,0 +1,14 @@
+package none;
+
+import org.junit.Test;
+
+/**
+ * @author peng-yongsheng
+ */
+public class ITTest {
+
+    @Test
+    public void test() {
+        System.out.println("INTEGRATION TESTS");
+    }
+}
