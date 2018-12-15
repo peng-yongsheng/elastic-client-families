766cf9f8c7079d98366b1b1144cf4a84f1b0af37
peng-yongsheng <pengys@apache.org>
2018-12-15 09:01:24 +0800

    Docker maven plugin test

---
+++ src/test/java/none/Test.java
@@ -0,0 +1,12 @@
+package none;
+
+/**
+ * @author peng-yongsheng
+ */
+public class Test {
+
+    @org.junit.Test
+    public void test() {
+        System.out.println("Running junit test....");
+    }
+}
