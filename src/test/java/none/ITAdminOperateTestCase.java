89f03bf8820d21768072e21267261dc190f9c8f2
peng-yongsheng <pengys@apache.org>
2018-12-17 00:57:11 +0800

    no message

--- src/test/java/none/ITAdminOperateTestCase.java
+++ src/test/java/none/ITAdminOperateTestCase.java
@@ -33,7 +33,7 @@ public class ITAdminOperateTestCase {
         settings.put("index.number_of_shards", 3);
         settings.put("index.number_of_replicas", 0);
 
-        ElasticClient client = new ElasticClient.Builder(new HostPort("localhost", 9200)).build();
+        ElasticClient client = new ElasticClient.Builder(new HostPort("127.0.0.1", 9200)).build();
         IndexCreateResponse indexCreateResponse = client.admin().create("admin_test_index", settings, new Mappings());
         Assert.assertEquals(200, indexCreateResponse.getCode());
         Assert.assertEquals("OK", indexCreateResponse.getMessage());
